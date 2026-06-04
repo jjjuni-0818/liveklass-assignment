3a3829c60827f046a44ab57605b13797e564b2f3
jjw920818-netizen <jjw920818@gmail.com>
2026-06-04 18:29:05 +0900

    fix: visualize.py 차트 저장 경로 수정 + queries.sql 주석 -- 으로 수정

--- sql/queries.sql
+++ sql/queries.sql
@@ -1,16 +1,16 @@
-# 이벤트 타입별 발생 횟수
+-- 쿼리 1: 이벤트 타입별 발생 횟수
 SELECT event_type, COUNT(*)
 FROM events
 GROUP BY event_type;
 
-# 유저별 총 이벤트 수
+-- 쿼리 2: 유저별 총 이벤트 수 (많은 순)
 SELECT user_id, COUNT(*)
 FROM events
 GROUP BY user_id
-ORDER BY COUNT(*) DESC; # 많은 순서대로 정렬
+ORDER BY COUNT(*) DESC;
 
-# 강의별 평균 시청 시간
-SELECT video_id, AVG(watch_seconds) 
+-- 쿼리 3: 강의별 평균 시청 시간 (긴 순)
+SELECT video_id, AVG(watch_seconds)
 FROM video_play_events
 GROUP BY video_id
 ORDER BY avg DESC;
